bd515f872d398d717c55ff07f428e177715ac961
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-11-04 19:07:02 +0500

    Created MUI theme component

---
+++ My Office Add-in/src/taskpane/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+        primary: { main: "#1E88E5" },
+        secondary: { main: "#FF9800" },
+    },
+    shape: { borderRadius: 10 },
+});
